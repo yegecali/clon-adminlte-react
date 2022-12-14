42579406b2d55ff37924d447156d4843af88e687
yegecali <yegecali2@gmail.com>
2022-12-14 18:31:05 -0500

    feat: setup styled components

---
+++ styles/Base.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+export const Title = styled.p`
+`
+export const Button = styled.button`
+`
---
+++ styles/Variants.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components;
+import { Button} from "./Base"
+
+const ButtonRounded = styled(Button)`
+`
